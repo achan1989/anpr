75c12d67af52711f265c9cd8b31f0820cba7e066
achan1989 <achan1989@gmail.com>
2017-12-16 21:59:04 +0000

    Can draw a random GeoJSON polygon on a map overlay.

--- web/js/map.js
+++ web/js/map.js
@@ -1,7 +1,26 @@
 var map;
-var ajaxRequest;
-var plotlist;
-var plotlayers=[];
+var svg;
+
+var geo_data = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [
+                [
+                    0.1246635,52.2055937
+                ],
+                [
+                    0.12147,52.2071275
+                ],
+                [
+                    0.1133752,52.1979725
+                ]
+            ]
+        }
+    }]
+}
 
 function initmap() {
     // set up the map
@@ -15,6 +34,44 @@ function initmap() {
     // start the map in Cambridge
     map.setView(new L.LatLng(52.205, 0.121),14);
     map.addLayer(osm);
+
+    svg = d3.select(map.getPanes().overlayPane).append("svg");
+    svg.attr("id", "svg_layer");
+    var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+    var transform = d3.geoTransform({point: projectPoint});
+    var path = d3.geoPath().projection(transform);
+
+    var feature = g.selectAll("path")
+        .data(geo_data.features)
+        .enter().append("path");
+
+
+    // Reposition the SVG to cover the features.
+    function reset() {
+        var bounds = path.bounds(geo_data),
+            topLeft = bounds[0],
+            bottomRight = bounds[1];
+
+        svg .attr("width", bottomRight[0] - topLeft[0])
+            .attr("height", bottomRight[1] - topLeft[1])
+            .style("left", topLeft[0] + "px")
+            .style("top", topLeft[1] + "px");
+
+        g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+        feature.attr("d", path);
+    }
+
+    map.on("viewreset", reset);
+    map.on("zoomend", reset);
+    reset();
+
+    // Use Leaflet to implement a D3 geometric transformation.
+    function projectPoint(x, y) {
+        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+    }
 }
 
 window.onload = initmap;
