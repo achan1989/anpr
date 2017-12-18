344aceb4da6567374b944389552f8c7edf9ce6fa
achan1989 <achan1989@gmail.com>
2017-12-18 22:20:30 +0000

    Refactor the map js.

--- anpr/web/static/js/map.js
+++ anpr/web/static/js/map.js
@@ -1,29 +1,18 @@
+console.log("start of map.js");
+
 var map;
 var svg;
-var trace_len = 20;
+var svg_grp;
 
-var geo_data = {
-    "type": "FeatureCollection",
-    "features": [{
-        "type": "Feature",
-        "geometry": {
-            "type": "LineString",
-            "coordinates": [
-                [
-                    0.1246635,52.2055937
-                ],
-                [
-                    0.12147,52.2071275
-                ],
-                [
-                    0.1133752,52.1979725
-                ]
-            ]
-        }
-    }]
-}
+var trace_len = 20;
+var data_chunks = [];
+
+var transform = d3.geoTransform({point: projectPoint});
+var path_gen = d3.geoPath().projection(transform);
+
 
 function initmap() {
+    console.log("initmap()");
     // set up the map
     map = new L.Map('mapid');
 
@@ -33,61 +22,70 @@ function initmap() {
     var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 14, attribution: osmAttrib});
 
     // start the map in Cambridge
-    map.setView(new L.LatLng(52.205, 0.121),14);
+    map.setView(new L.LatLng(52.205, 0.121),13);
     map.addLayer(osm);
 
     svg = d3.select(map.getPanes().overlayPane).append("svg");
     svg.attr("id", "svg_layer");
-    var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+    svg_grp = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-    var transform = d3.geoTransform({point: projectPoint});
-    var path = d3.geoPath().projection(transform);
-
-    // Use Leaflet to implement a D3 geometric transformation.
-    function projectPoint(x, y) {
-        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-        this.stream.point(point.x, point.y);
-    }
-
-    var feature = g.selectAll("path")
-        .data(geo_data.features)
-        .enter().append("path");
-
-
-    // Reposition the SVG to cover the features.
-    function reset() {
-        var bounds = path.bounds(geo_data),
-            topLeft = bounds[0],
-            bottomRight = bounds[1];
-
-        svg .attr("width", bottomRight[0] - topLeft[0])
-            .attr("height", bottomRight[1] - topLeft[1])
-            .style("left", topLeft[0] + "px")
-            .style("top", topLeft[1] + "px");
-
-        g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-
-        feature.attr("d", path);
-        feature.classed("trip_path", true);
-    }
+    setTimeout(function(){load_chunk_for_hour(0);}, 5000);
 
     map.on("viewreset", reset);
     map.on("zoomend", reset);
     reset();
-
-    svg.select("path").call(transition);
 }
 
 window.onload = initmap;
 
-function transition(path) {
-    path.attr("stroke-dasharray", trace_len + "," + path.node().getTotalLength());
+// Reposition the SVG to cover the features.
+function reset() {
+    console.log("reset()");
+    if (data_chunks.length == 0) {
+        return;
+    }
+
+    var bounds = path_gen.bounds(data_chunks[0]),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+
+    svg .attr("width", bottomRight[0] - topLeft[0])
+        .attr("height", bottomRight[1] - topLeft[1])
+        .style("left", topLeft[0] + "px")
+        .style("top", topLeft[1] + "px");
+
+    svg_grp .attr(
+        "transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+    svg_grp.selectAll("path").attr("d", path_gen);
+}
+
+function load_chunk_for_hour(hour) {
+    console.log("starting load");
+    d3.json("/api/by-hour/day0/0/data.geojson", function(data){
+        console.log("Loaded geojson");
+        data_chunks.push(data);
+
+        var feature = svg_grp.selectAll("path")
+            .data(data.features)
+            .enter().append("path")
+                .attr("d", path_gen)
+                .classed("trip_path", true);
+
+        reset();
+        svg.selectAll("path").each(transition);
+    });
+}
+
+function transition(d, i, nodes) {
+    var path = d3.select(this);
+    path.attr("stroke-dasharray", trace_len + "," + this.getTotalLength());
     path.attr("stroke-dashoffset", trace_len);
     path.transition()
-        .duration(7500)
+        .duration(60000)
         .ease(d3.easeLinear)
-        .attrTween("stroke-dashoffset", tweenDash)
-        .on("end", function() { d3.select(this).call(transition); });// infinite loop
+        .attrTween("stroke-dashoffset", tweenDash);
+        //.on("end", function() { d3.select(this).call(transition); });// infinite loop
 }
 
 function tweenDash() {
@@ -103,4 +101,10 @@ function tweenDash() {
         return i(t);
     }
     */
+}
+
+// Use Leaflet to implement a D3 geometric transformation.
+function projectPoint(x, y) {
+    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
 }
